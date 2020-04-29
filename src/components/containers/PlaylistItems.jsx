1e318da6a6f4f708f67db931b0e4bb8a83dae6a0
Elizabeth Villalejos <nekobanpaia@gmail.com>
2020-04-28 17:09:35 -0700

    Updating PlaylistItems component with props

--- src/components/containers/PlaylistItems.jsx
+++ src/components/containers/PlaylistItems.jsx
@@ -2,9 +2,16 @@ import React from 'react';
 import PlaylistItem from '../PlaylistItem';
 import StyledPlaylistItems from '../styles/StyledPlaylistItems';
 
-const PlaylistItems = (props) => (
+const PlaylistItems = ({ videos, active }) => (
   <StyledPlaylistItems>
-    <PlaylistItem />
+    {videos.map(video=>(
+      <PlaylistItem
+        key={video.id}
+        video={video}
+        active={video.id === active.id ? true : false}
+        played={video.played}
+      />
+    ))}
   </StyledPlaylistItems>
 );
 
