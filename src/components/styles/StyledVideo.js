df54b3c2e682e677eae3b55484fd71520cbffba6
Elizabeth Villalejos <nekobanpaia@gmail.com>
2020-04-30 13:53:35 -0700

    Updating readme with screenshots and updating videoplaylist

--- src/components/styles/StyledVideo.js
+++ src/components/styles/StyledVideo.js
@@ -4,7 +4,8 @@ const StyledVideo = styled.div`
   position: relative;
   -webkit-box-flex: 2;
   -ms-flex: 2 0 900px;
-  flex: 2 0 900px;
+  top: 0.2rem;
+  flex: 1 0 70vw;
 
   @media screen and (max-width: 1400px) {
     width: 100%;
