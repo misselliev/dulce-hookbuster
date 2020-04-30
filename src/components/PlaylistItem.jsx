8e3fe9ea72ec91e10a42296afa1d69708f7ea2d0
Elizabeth Villalejos <nekobanpaia@gmail.com>
2020-04-30 13:06:06 -0700

    Fixing prop names and styling for PlaylistItem and PlaylistItems

--- src/components/PlaylistItem.jsx
+++ src/components/PlaylistItem.jsx
@@ -4,7 +4,7 @@ import StyledPlaylistItem from './styles/StyledPlaylistItem';
 const PlaylistItem = ({ video, active, played }) => (
   <StyledPlaylistItem active={active} played={played}>
     <div className="woof-player__video-nr">{video.num}</div>
-    <div className="woof-player__video-name">{video.titles}</div>
+    <div className="woof-player__video-name">{video.title}</div>
     <div className="woof-player__video-time">{video.duration}</div>
   </StyledPlaylistItem>
 );
